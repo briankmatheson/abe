31f286dd9e80240b2e595ab05c6e93e124a2bf71
Brian Matheson <bmath@brianmatheson.com>
2025-05-15 02:48:03 -0400

    add simple client

---
+++ extra/client.sh
@@ -0,0 +1,22 @@
+#!/bin/bash -xe
+host=brick
+url=http://$host/configure
+
+sudo sh -c 'apt update && apt -y install curl nvme-cli nfs-kernel-server' || exit 1
+sudo modprobe nvmet
+
+response=`curl --silent $url`
+message=`echo $response | jq -r .message`
+
+while read line; do
+    result=`echo $line | bash`
+    id=`echo $line | awk '{print $NF}'`
+    device=`sudo /sbin/blkid -t PARTLABEL=$id | awk -F: '{print $1}'`
+    sudo mkdir -p /export/$id
+    sudo mount $device /export/$id
+    echo "/export/$id		*(rw,sync,fsid=0,no_root_squash,no_subtree_check)" | sudo tee -a /etc/exports
+    sudo exportfs -a
+done <<EOF
+$message
+EOF
+
