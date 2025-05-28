cf7ed0188f1c2ddd6735d9812a678c19ec6e3066
Brian Matheson <bmath@brianmatheson.com>
2025-05-28 00:28:04 -0400

    add cheezy persistence, needs error checking

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,22 @@
+use nix::libc::int16_t;
 use nix::mount::{MsFlags, mount};
 use nix::unistd::symlinkat;
 use std::path::PathBuf;
-use std::fs::{self, File};
-use std::io::{Result, Write, Error, ErrorKind};
+use std::fs::{self, File, write};
+use std::io::{Result, Write, Read, Error, ErrorKind};
+use std::fmt::Display;
 use std::path::Path;
 use std::process::Command;
 use std::env::var;
 use std::net::Ipv4Addr;
 use std::net::SocketAddr;
+use std::str;
 use uuid::Uuid;
 use ipnet::Ipv4Net;
 use env_logger::Env;
 use log::{info, warn};
 use axum::{
+    extract::Path as EP,
     routing::{get, post},
     Json, Router
 };
@@ -23,6 +27,7 @@ use serde::{Deserialize, Serialize};
 const CONFIGFS_PATH: &str = "/sys/kernel/config";
 const NVMET_PATH: &str = "/sys/kernel/config/nvmet";
 const PREFIX: &str = "172.23.23.0/24";
+const VOL_SIZE: &str = "100G";
 
 //env_inventory::register!(RUST_LOG = "info")
 
@@ -43,6 +48,63 @@ async fn echo(Json(payload): Json<Message>) -> Json<Message> {
     })
 }
 
+async fn configure() -> Json<Message> {
+    let id: String = Uuid::new_v4().to_string().replace("-", "");
+    create_lv(&id, VOL_SIZE).unwrap(); 
+    info!("attach id is {}", id); 
+    do_attach(id).await
+}
+
+async fn attach(EP(id): EP<String>) -> Json<Message> {
+   
+    info!("attach id is {}", id);  
+    do_attach(id).await
+}
+	
+async fn do_attach(id: String) -> Json<Message> {
+    let numpaths = fs::read_dir(format!("{NVMET_PATH}/ports")).unwrap().count();
+    info!("numpaths is {}", numpaths);
+
+    let mut last_port: &str = "0";
+    let latest_entry: PathBuf;
+    if numpaths > 0 {
+        let first: fs::DirEntry =
+            fs::read_dir(format!("{NVMET_PATH}/ports")).unwrap().nth(0).unwrap().unwrap();
+        latest_entry = first.path();
+        last_port = latest_entry.file_name().unwrap().to_str().unwrap();
+    }
+    info!("last_port is {}", last_port);
+
+    let i = last_port.parse::<u32>().unwrap() + 1;
+    info!("i is {}", i);
+
+    let lv_path = lv_path_for_uuid(&id);
+    
+    let target = Subsystem::create(&id).await.unwrap();
+    target.add_namespace("1", &lv_path).await.unwrap();
+
+    let svc_port = format!("44{:03}", i);
+    info!("svc_port is {svc_port}");
+	
+
+    let port  = Port::create(id.clone(), &svc_port, "tcp", i).unwrap();
+    port.link_subsystem(&target);
+	
+    println!("{} {}:{}", port.id, port.traddr, port.trsvcid);
+    let output = Command::new("ufw")
+        .args(["allow", &port.trsvcid])
+        .output().unwrap();
+    info!("{:?}", output);
+    
+    Json(Message {
+        message: format!("sudo nvme discover -a {} -t tcp -s {}; sudo nvme connect -a {} -t tcp -s {} -n {}",
+			 port.traddr,
+			 port.trsvcid,
+			 port.traddr,
+			 port.trsvcid,
+			 id),
+    })
+}
 
 // NVME over TCP target subsystem
 struct Subsystem {
@@ -52,29 +114,29 @@ impl Subsystem {
     async fn create(name: &str) -> Result<Self> {
         let path = format!("{}/subsystems/{}", NVMET_PATH, name);
 
-        info!("making nvmet subsystem dif");
+        info!("making nvmet subsystem dir");
         fs::create_dir_all(&path)?;
 
         info!("allowing any host");
-        File::create(format!("{}/attr_allow_any_host", path))?.write_all(b"1")?;
+        fs::write(format!("{}/attr_allow_any_host", path), "1");
         Ok(Self {
             name: name.to_string(),
         })
     }
-    fn add_namespace(&self, nsid: &str, device_path: &str) -> Result<()> {
+    async fn add_namespace(&self, nsid: &str, device_path: &str) -> Result<()> {
         let ns_path = format!(
             "{}/subsystems/{}/namespaces/{}",
             NVMET_PATH, self.name, nsid
         );
         info!("making namespace {ns_path}");     
-        fs::create_dir_all(&ns_path)?;
+        fs::create_dir_all(&ns_path);
 
         info!("adding device {device_path}");
-        File::create(format!("{}/device_path", ns_path))?.write_all(device_path.as_bytes())?;
+        File::create(format!("{}/device_path", ns_path))?.write_all(device_path.as_bytes());
 
         
         info!("enabling {ns_path}/enable");
-        File::create(format!("{}/enable", ns_path))?.write_all(b"1")?;
+        File::create(format!("{}/enable", ns_path))?.write_all(b"1");
         Ok(())
     }
 }
@@ -87,31 +149,39 @@ struct Port {
     iteration: u32
 }
 impl Port {
-    fn create(id: String, trsvcid: &str, trtype: &str, iteration: u32) -> Result<Port> {    
+    fn create(id: String, svcid: &str, trtype: &str, iteration: u32) -> Result<Port> {    
         let path = format!("{}/ports/{}", NVMET_PATH, iteration);
 
         info!("making port {path}");
-        fs::create_dir_all(&path)?;
-        let traddr: String = "192.168.1.24".to_string();
-        
-        info!("writing addr_traddr {path}");
-        File::create(format!("{}/addr_traddr", path))?.write_all(traddr.as_bytes())?;
-
-        info!("writing addr_trsvcid{path}");
-        File::create(format!("{}/addr_trsvcid", path))?.write_all(trsvcid.as_bytes())?;
-
-        info!("making addr_trtype {path}");
-        File::create(format!("{}/addr_trtype", path))?.write_all(trtype.as_bytes())?;
-
-        info!("making addr_adrfam {path}");
-        File::create(format!("{}/addr_adrfam", path))?.write_all("ipv4".as_bytes())?;
+	let traddr: String = "192.168.1.24".to_string();
+	let mut trsvcid: String = svcid.to_string();
+        let result = fs::create_dir_all(&path);
+	if result.is_ok() {
+            info!("writing addr_traddr {path}");
+            File::create(format!("{}/addr_traddr", path))?.write_all(traddr.as_bytes())?;
+	    
+            info!("writing addr_trsvcid{path}");
+            File::create(format!("{}/addr_trsvcid", path))?.write_all(trsvcid.as_bytes())?;
+	    
+            info!("making addr_trtype {path}");
+            File::create(format!("{}/addr_trtype", path))?.write_all(trtype.as_bytes())?;
+	    
+            info!("making addr_adrfam {path}");
+            File::create(format!("{}/addr_adrfam", path))?.write_all("ipv4".as_bytes())?;
+	} else {
+	    // find the old stuff
+	    let mut buffer: [u8; 1024] = [0; 1024];
+	    File::open("{path}/addr_trsvcid")?.read(&mut buffer)?;
+	    let trsvcid = str::from_utf8_mut(&mut buffer).unwrap();
+	}
         Ok(Port{
-            id: id, 
-            traddr: traddr, 
-            trsvcid: trsvcid.to_string(), 
-            iteration: iteration})
+	    id: id, 
+	    traddr: traddr, 
+	    trsvcid: trsvcid, 
+	    iteration: iteration
+	})
     }
-
+    
     fn link_subsystem(&self, subsystem: &Subsystem) -> Result<()> {
         let subsys_path: String = format!("{}/subsystems/{}", NVMET_PATH, subsystem.name);
         let port_subsys_link: String = format!(
@@ -195,53 +265,6 @@ pub fn create_lv(name: &str, size: &str) -> Result<()> {
     Ok(())
 }    
 
-async fn configure() -> Json<Message> {
-    let numpaths = fs::read_dir(format!("{NVMET_PATH}/ports")).unwrap().count();
-    info!("numpaths is {}", numpaths);
-
-    let mut last_port: &str = "0";
-    let latest_entry: PathBuf;
-    if numpaths > 0 {
-        let first: fs::DirEntry =
-            fs::read_dir(format!("{NVMET_PATH}/ports")).unwrap().nth(0).unwrap().unwrap();
-        latest_entry = first.path();
-        last_port = latest_entry.file_name().unwrap().to_str().unwrap();
-    }
-    info!("last_port is {}", last_port);
-
-    let i = last_port.parse::<u32>().unwrap() + 1;
-    info!("i is {}", i);
-    
-    let uuid: String = Uuid::new_v4().to_string().replace("-", "");
-    create_lv(&uuid, "1G").unwrap();
-
-    let lv_path = lv_path_for_uuid(&uuid);
-    
-    let target = Subsystem::create(&uuid).await.unwrap();
-    target.add_namespace("1", &lv_path).unwrap();
-    
-    let svc_port = format!("44{:03}", i);
-    info!("svc_port is {svc_port}");
-
-
-    let port = Port::create(uuid.clone(), &svc_port, "tcp", i).unwrap();
-    port.link_subsystem(&target);
-
-    println!("{} {}:{}", port.id, port.traddr, port.trsvcid);
-    let output = Command::new("ufw")
-        .args(["allow", &port.trsvcid])
-        .output().unwrap();
-    info!("{:?}", output);
-        
-    Json(Message {
-        message: format!("sudo nvme discover -a {} -t tcp -s {}; sudo nvme connect -a {} -t tcp -s {} -n {}",
-			 port.traddr,
-			 port.trsvcid,
-			 port.traddr,
-			 port.trsvcid,
-			 uuid),
-    })
-}
 
 #[tokio::main]
 async fn main() {
@@ -251,7 +274,8 @@ async fn main() {
     ensure_nvmet_present();
 
     let app = Router::new()
-        .route("/", get(hello))
+        .route("/id/:id", get(attach))
+        .route("/hello", get(hello))
         .route("/echo", post(echo))
         .route("/configure", get(configure));
 
